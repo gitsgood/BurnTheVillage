665d4592c58e39db58b4372f94a644b5a3955f10
gitsgood <christoforoskhouri@gmail.com>
2025-10-09 04:44:13 +0200

    Initial commit

---
+++ Source/BurnTheVillage/BurnTheVillage.Build.cs
@@ -0,0 +1,46 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class BurnTheVillage : ModuleRules
+{
+	public BurnTheVillage(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"NavigationSystem",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"Niagara",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"BurnTheVillage",
+			"BurnTheVillage/Variant_Strategy",
+			"BurnTheVillage/Variant_Strategy/UI",
+			"BurnTheVillage/Variant_TwinStick",
+			"BurnTheVillage/Variant_TwinStick/AI",
+			"BurnTheVillage/Variant_TwinStick/Gameplay",
+			"BurnTheVillage/Variant_TwinStick/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/BurnTheVillage/BurnTheVillage.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BurnTheVillage.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BurnTheVillage, "BurnTheVillage" );
+
+DEFINE_LOG_CATEGORY(LogBurnTheVillage)
+ 
